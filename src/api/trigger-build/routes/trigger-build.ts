4ceea0d3c33ec3f718f2f4fc8f2c4a1ffe9b9759
Pramoj Mohanan <pramoj.mohanan@numentica-ui.com>
2024-05-26 22:03:17 +0530

    triggerbuild true to post

--- src/api/trigger-build/routes/trigger-build.ts
+++ src/api/trigger-build/routes/trigger-build.ts
@@ -10,7 +10,7 @@ export default {
       },
     },
     {
-      method: 'GET',
+      method: 'POST',
       path: '/trigger-build/true',
       handler: 'trigger-build.triggerBuildTrue',
       config: {
