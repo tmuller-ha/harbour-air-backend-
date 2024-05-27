dc755d21680b50468cb1331f928dc8595085f8ca
Pramoj Mohanan <104882683+pramojmohanan@users.noreply.github.com>
2024-05-27 14:26:13 +0530

    Merge dev (#7)
    * Workflow/build changes (#6)
    
    * dev build with self hosted runner
    
    * dev deploy workflow
    
    * Build changes
    
    * fix
    
    * added pg
    
    * data base config updates
    
    * db changes
    
    * ssl issue database
    
    * Test content deployment
    
    * Admin panel customization and S3 for file upload
    
    * env chanes
    
    * changes
    
    * changes 01
    
    * work flow fix
    
    * fix
    
    * database url connection
    
    * removed test content
    
    * Custom trigger build for frontend and home single type
    
    * trigger build schema change
    
    * Trigger api visible to false
    
    * triggerbuild true to post

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,26 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SeoSeo extends Schema.Component {
+  collectionName: 'components_seo_seos';
+  info: {
+    displayName: 'SEO';
+    icon: 'search';
+  };
+  attributes: {
+    metaKeyword: Attribute.String;
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.String;
+    canonicalUrl: Attribute.String;
+    ogImage: Attribute.Media;
+    markAsCanonical: Attribute.Boolean;
+    noFollowNoIndex: Attribute.Boolean;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'seo.seo': SeoSeo;
+    }
+  }
 }
