40bbf23a7bcf072d3fcf7a6666799547ed420048
BharaniNUI <bharanidaran.g@numentica-ui.com>
2024-06-03 11:58:33 +0530

    added eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
       'plugin:@typescript-eslint/recommended',
+      'plugin:import/recommended'
     ],
+    plugins: ["@typescript-eslint", "import"],
     parserOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
@@ -17,6 +19,34 @@ module.exports = {
           extendDefaults: true,
         },
       ],
-      '@typescript-eslint/no-unused-vars': 'warn'
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'import/no-duplicates': ["warn", {"considerQueryString": true}],
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            ['builtin', 'external'], 
+            'internal', 
+            ['parent', 'sibling'], 
+            'index', 
+            'object', 
+            'type', 
+            'unknown', 
+          ],
+          pathGroups: [
+            {
+              pattern: 'react|next',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react', 'next'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'never',
+        },
+      ],
     },
   };
