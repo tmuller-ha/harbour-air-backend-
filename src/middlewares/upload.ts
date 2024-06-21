999364100369ffe84220c73b4ef2762c480c1cb1
Pramoj Mohanan <104882683+pramojmohanan@users.noreply.github.com>
2024-06-21 11:27:51 +0530

    Pm/upload image type to webp (#23)
    * Upload image type to webp
    
    * image webp converter
    
    * Os specific fix
    
    * Webp convert fix
    
    * Webp convertion

---
+++ src/middlewares/upload.ts
@@ -0,0 +1,61 @@
+const path = require("path");
+const fs = require("fs").promises;
+const os = require("os");
+const webp = require("webp-converter");
+
+module.exports = (config, { strapi }) => {
+  return async (context, next) => {
+    if (
+      context.request.method === "POST" &&
+      context.request.url.startsWith("/upload")
+    ) {
+      webp.grant_permission();
+      // Function to convert images to WebP format
+      const convertImageToWebP = async (file) => {
+        if (file.type.startsWith("image/")) {
+          try {
+            const outputName = `${path.basename(
+              file.name,
+              path.extname(file.name)
+            )}.webp`;
+            const outputDir = os.tmpdir(); // Use OS-specific temporary directory
+            const outputPath = path.join(outputDir, outputName);
+
+            // Convert image to WebP format using webp-converter
+            await webp.cwebp(file.path, outputPath, "-q 80");
+
+            // Delete the original file
+            await fs.unlink(file.path);
+
+            // Update file properties
+            context.request.body.fileInfo = { name: outputName };
+            file.path = outputPath;
+            file.name = outputName;
+            file.type = "image/webp";
+          } catch (error) {
+            console.error("Error converting image to WebP", error);
+            throw error; // Re-throw the error to ensure it gets logged properly
+          }
+        }
+      };
+
+      // Convert images and handle next middleware
+      try {
+        if (context.request.files && context.request.files.files) {
+          const files = context.request.files.files;
+          if (Array.isArray(files)) {
+            await Promise.all(files.map(convertImageToWebP));
+          } else {
+            await convertImageToWebP(files);
+          }
+        }
+        await next();
+      } catch (error) {
+        console.error("Error processing upload", error);
+        context.throw(500, "Error processing upload");
+      }
+    } else {
+      await next();
+    }
+  };
+};
