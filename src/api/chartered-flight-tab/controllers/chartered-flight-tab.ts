1056faff321812797bd8d0510b91bd2c315d09b7
Pramoj Mohanan <104882683+pramojmohanan@users.noreply.github.com>
2024-07-17 15:34:52 +0530

    Revert "Chartered page Schema created (#38)"
    This reverts commit 45b4145709149e525fcdb348ec8a05f714f643c7.

--- src/api/chartered-flight-tab/controllers/chartered-flight-tab.ts
+++
@@ -1,7 +0,0 @@
-/**
- * chartered-flight-tab controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::chartered-flight-tab.chartered-flight-tab');
--- src/api/table/controllers/table.ts
+++
@@ -1,7 +0,0 @@
-/**
- * table controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::table.table');
