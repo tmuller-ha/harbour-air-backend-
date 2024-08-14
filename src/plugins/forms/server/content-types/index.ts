05ac394a4f6b8883b781e43a1ce50e30861904d9
Rekaa-Srinivasan <rekaa.srinivasan@numentica-ui.com>
2024-08-14 18:14:20 +0530

    collection type and API for 'Request a Quote' form of Parcel Express page

--- src/plugins/forms/server/content-types/index.ts
+++ src/plugins/forms/server/content-types/index.ts
@@ -1,9 +1,11 @@
 import charteredFlightRequest from "./chartered-flight-request";
 import tourRequest from "./tour-request";
 import contactForm from "./contact-form";
+import parcelExpressQuote from "./parcel-express-quote";
 
 export default {
     "tour-request" : tourRequest,
     "chartered-flight-request" : charteredFlightRequest,
     "contact-form" : contactForm,
+    "parcel-express-quote" : parcelExpressQuote,
 };
