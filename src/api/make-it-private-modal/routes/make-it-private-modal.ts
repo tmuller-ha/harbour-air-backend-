212133978774982d9927f25ad8ccc1dd28499b42
Rekaa-Srinivasan <162275345+Rekaa-Srinivasan@users.noreply.github.com>
2024-08-23 10:51:50 +0530

    cms integration for make-it-private modal (#105)

---
+++ src/api/make-it-private-modal/routes/make-it-private-modal.ts
@@ -0,0 +1,7 @@
+/**
+ * make-it-private-modal router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::make-it-private-modal.make-it-private-modal');
