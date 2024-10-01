e393345effbcffc4ff91d1256fb7a2e20815eb2f
Iniyavan22 <iniyavan.b@numentica-ui.com>
2024-10-01 12:59:59 +0530

    Updated Email forms api

--- src/api/email-form/controllers/email-form.ts
+++ src/api/email-form/controllers/email-form.ts
@@ -16,6 +16,7 @@ import {
   parcelExpressQuoteTemplate,
   turbobucksOrderTemplate,
 } from "./email-templates/index";
+import { pick } from "lodash";
 import { EmailFormType, Form } from "./formType";
 
 export default factories.createCoreController(
@@ -26,204 +27,221 @@ export default factories.createCoreController(
         const body = ctx.request.body as any;
         const { formType } = body;
 
-        const emailForm =await strapi
+        const emailForm = await strapi
           .documents("api::email-form.email-form")
           .findFirst({
             status: "published",
             populate: [
               "contactDetailsForm.toEmails",
               "loyaltyDetailsForm.toEmails",
-              
             ],
           });
 
         switch (formType) {
           case Form.contactDetailsForm:
             const { contactDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of contactDetailsForm?.toEmails || []){
+            for (const userDetail of contactDetailsForm?.toEmails || []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
-                      once production access is added we need to update with body.email 
+                      once production access is added we need to update with body.email \
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 contactFormTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
-            }           
+              );
+            }
           case Form.baggageDetailsForm:
             const { baggageDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of baggageDetailsForm?.toEmails || []){
+            for (const userDetail of baggageDetailsForm?.toEmails || []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 baggageClaimFormRequestTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.carServiceDetailsForm:
             const { carServiceDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of carServiceDetailsForm?.toEmails || []){
+            for (const userDetail of carServiceDetailsForm?.toEmails || []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 carServiceFormRequestTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.charteredFlightDetailsForm:
             const { charteredFlightDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of charteredFlightDetailsForm?.toEmails || []){
+            for (const userDetail of charteredFlightDetailsForm?.toEmails ||
+              []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 charteredFlightRequestTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.inquiriesDetailsForm:
             const { inquiriesDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of inquiriesDetailsForm?.toEmails || []){
+            for (const userDetail of inquiriesDetailsForm?.toEmails || []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 inquiriesFormRequestTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.onAccountTicketDetailsForm:
             const { onAccountTicketDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of onAccountTicketDetailsForm?.toEmails || []){
+            for (const userDetail of onAccountTicketDetailsForm?.toEmails ||
+              []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 onAccountTicketTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.parcelExpressQuoteDetailsForm:
-            const { parcelExpressQuoteDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of parcelExpressQuoteDetailsForm?.toEmails || []){
+            const { parcelExpressQuoteDetailsForm } =
+              emailForm as EmailFormType;
+            for (const userDetail of parcelExpressQuoteDetailsForm?.toEmails ||
+              []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 parcelExpressQuoteTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.discountedQuoteDetailsForm:
             const { discountedQuoteDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of discountedQuoteDetailsForm?.toEmails || []){
+            for (const userDetail of discountedQuoteDetailsForm?.toEmails ||
+              []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 discountedTravelRequestTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.groupDetailsForm:
             const { groupDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of groupDetailsForm?.toEmails || []){
+            for (const userDetail of groupDetailsForm?.toEmails || []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 groupBookingsFormRequestTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.turbobucksOrderDetailsForm:
             const { turbobucksOrderDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of turbobucksOrderDetailsForm?.toEmails || []){
+            for (const userDetail of turbobucksOrderDetailsForm?.toEmails ||
+              []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 turbobucksOrderTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
           case Form.loyaltyDetailsForm:
             const { loyaltyDetailsForm } = emailForm as EmailFormType;
-            for (const userDetail of loyaltyDetailsForm?.toEmails || []){
+            for (const userDetail of loyaltyDetailsForm?.toEmails || []) {
               await strapi.plugins["email"].services.email.sendTemplatedEmail(
                 {
                   to: process.env.DEFAULT_REPLY_TO_EMAIL,
                   /** Currently we don't have production access for ses 
                       so setting default to email for emails
                       once production access is added we need to update with body.email 
+                      TODO: update to email to the userDetails.email later
                     */
                 },
                 highFlyerRewardsApplicationTemplate,
                 {
-                  user: userDetail,
+                  user: pick(body, ["email"]),
                 }
-              )
+              );
             }
         }
         return emailForm;
