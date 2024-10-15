8ff2e2d330b3ab2192a1d93cb186fc13209f3659
Pramoj Mohanan <104882683+pramojmohanan@users.noreply.github.com>
2024-10-15 21:30:25 +0530

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -4,5 +4,5 @@ export default ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  // url: env("CMS_URL", "https://cms.hbair.biz"),
+   url: env("CMS_URL", "https://cms.hbair.biz"),
 });
