7e586646b7f8796e113bb5202f23d3fd600f8ca4
Iniyavan22 <iniyavan.b@numentica-ui.com>
2025-01-02 17:27:52 +0530

    Status param added

--- config/admin.ts
+++ config/admin.ts
@@ -5,107 +5,107 @@ const getPreviewPathname = (uid, { locale, document }): string => {
     case "api::home.home":
       return "/";
     case "api::about-us.about-us":
-      return "/about-us/";
+      return "about-us";
     case "api::air-craft-fleet.air-craft-fleet":
-      return "/aircraft-fleet/";
+      return "aircraft-fleet";
     case "api::award.award":
-      return "/about-us/awards/";
+      return "about-us/awards";
     case "api::blogs-and-new.blogs-and-new":
-      return "/blogs-news/";
+      return "blogs-news";
     case "api::book-flight.book-flight":
-      return "/book-flight/";
+      return "book-flight";
     case "api::canadian-passenger-right.canadian-passenger-right":
-      return "/tariff/";
+      return "tariff";
     case "api::car-service.car-service":
-      return "/car-services/";
+      return "car-services";
     case "api::career.career":
-      return "/careers/";
+      return "careers";
     case "api::chartered-flight.chartered-flight":
-      return "/chartered-flight/";
+      return "chartered-flight";
     case "api::community-involvement.community-involvement":
-      return "/community-involvement/";
+      return "community-involvement";
     case "api::contact-us.contact-us":
-      return "/contact-us/";
+      return "contact-us";
     case "api::corporate-resposibility.corporate-resposibility":
-      return "/corporate-responsibility/";
+      return "corporate-responsibility";
     case "api::courtesy-shuttle.courtesy-shuttle":
-      return "/courtesy-shuttles/";
+      return "courtesy-shuttles";
     case "api::faq.faq":
-      return "/faq/";
+      return "faq";
     case "api::flight-status.flight-status":
-      return "/flight-status/";
+      return "flight-status";
     case "api::going-electric.going-electric":
-      return "/going-electric/";
+      return "going-electric";
     case "api::group-booking.group-booking":
-      return "/group-bookings/";
+      return "group-bookings";
     case "api::high-flyer-reward.high-flyer-reward":
-      return "/high-flyer-rewards/";
+      return "high-flyer-rewards";
     case "api::loyalty-program.loyalty-program":
-      return "/loyalty-programs/";
+      return "loyalty-programs";
     case "api::luggage.luggage":
-      return "/luggage/";
+      return "luggage";
     case "api::main-deal.main-deal":
-      return "/deals/";
+      return "deals";
     case "api::mobility.mobility":
-      return "/extra-assistance/mobility/";
+      return "extra-assistance/mobility";
     case "api::moving-past-carbon-neutral.moving-past-carbon-neutral":
-      return "/moving-past-carbon-neutral/";
+      return "moving-past-carbon-neutral";
     case "api::not-found-page.not-found-page":
-      return "/not-found/";
+      return "not-found";
     case "api::on-account-quick-ticket.on-account-quick-ticket":
-      return "/on-account-quick-tickets/";
+      return "on-account-quick-tickets";
     case "api::our-fare.our-fare":
-      return "/fares/";
+      return "fares";
     case "api::our-promise.our-promise":
-      return "/about-us/our-promises/";
+      return "about-us/our-promises";
     case "api::our-story.our-story":
-      return "/about-us/our-timeline/";
+      return "about-us/our-timeline";
     case "api::parcel-express.parcel-express":
-      return "/parcel-express/";
+      return "parcel-express";
     case "api::private-tour.private-tour":
-      return "/private-tours/";
+      return "private-tours";
     case "api::safety-first.safety-first":
-      return "/safety-first/";
+      return "safety-first";
     case "api::schedule.schedule":
-      return "/schedules/";
+      return "schedules";
     case "api::seating-option.seating-option":
-      return "/seating-options/";
+      return "seating-options";
     case "api::standby-travel.standby-travel":
-      return "/standby-travel/";
+      return "standby-travel";
     case "api::turbobucks-flight-credit.turbobucks-flight-credit":
-      return "/turbobucks-flight-credits/";
+      return "turbobucks-flight-credits";
     case "api::base-page.base-page":
       return `${slug}`;
     case "api::extra-assistance.extra-assistance":
     case "api::assistance.assistance": {
       if (!slug) {
-        return "/extra-assistance/";
+        return "extra-assistance";
       }
-      return `/extra-assistance/${slug}`;
+      return `extra-assistance/${slug}`;
     }
     case "api::blog-list.blog-list": {
       if (!slug) {
-        return "/blog";
+        return "blog";
       }
-      return `/blog/${slug}`;
+      return `blog/${slug}`;
     }
     case "api::news-list.news-list": {
       if (!slug) {
-        return "/news";
+        return "news";
       }
-      return `/news/${slug}`;
+      return `news/${slug}`;
     }
     case "api::main-location.main-location": {
       if (!slug) {
-        return "/locations/";
+        return "locations";
       }
-      return `/locations/${slug}`;
+      return `locations/${slug}`;
     }
     case "api::main-tour.main-tour": {
       if (!slug) {
-        return "/tours/";
+        return "tours";
       }
-      return `/tours/${slug}`;
+      return `tours/${slug}`;
     }
     default: {
       return null;
@@ -136,8 +136,8 @@ export default ({ env }) => ({
       async handler(uid, { documentId, locale, status }) {
         const document = await strapi.documents(uid).findOne({ documentId });
         const pathname = getPreviewPathname(uid, { locale, document });
-
-        return `${env("WEB_PREVIEW_URL")}${pathname}`;
+        const url = `${env("WEB_PREVIEW_URL")}/api/draft?slug=${pathname}&status=${status}`;
+        return url;
       },
     },
   },
