3887f2228177262d80258ea4b4bc97e73ee28520
Pramoj Mohanan <104882683+pramojmohanan@users.noreply.github.com>
2025-03-26 21:38:15 +0530

    Revert "Sa/careerbackend"

--- src/api/aerospace-career/routes/aerospace-career.ts
+++
@@ -1,7 +0,0 @@
-/**
- * aerospace-career router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::aerospace-career.aerospace-career');
--- src/api/aerospace-career/services/aerospace-career.ts
+++
@@ -1,7 +0,0 @@
-/**
- * aerospace-career service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::aerospace-career.aerospace-career');
