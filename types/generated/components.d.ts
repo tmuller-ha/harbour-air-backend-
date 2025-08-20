4b86daf36e86b26a1c6e3c060fd53e38b92f6063
Pramoj Mohanan <104882683+pramojmohanan@users.noreply.github.com>
2025-08-20 15:28:13 +0530

    hero alignment

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1214,6 +1214,8 @@ export interface HomeHomeCarousel extends Struct.ComponentSchema {
     displayName: 'HomeCarousel';
   };
   attributes: {
+    alignContentLeft: Schema.Attribute.Boolean &
+      Schema.Attribute.DefaultTo<false>;
     btnText: Schema.Attribute.String;
     description: Schema.Attribute.String;
     desktopImg: Schema.Attribute.Media<'images' | 'videos'>;
